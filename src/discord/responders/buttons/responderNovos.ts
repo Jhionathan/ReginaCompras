215aac2ef2aeb5875cff2fc54f42c17a98919f23
jhionathan <jhionathanb@outlook.com>
2024-11-26 09:09:57 -0300

    novos prod

--- src/discord/responders/buttons/responderNovos.ts
+++ src/discord/responders/buttons/responderNovos.ts
@@ -1,7 +1,14 @@
 import { Responder, ResponderType } from "#base";
 import { createModalFields } from "@magicyan/discord";
-import { ModalSubmitInteraction, TextInputStyle } from "discord.js";
+import {  TextChannel, Routes, TextInputStyle, REST, ModalSubmitInteraction } from "discord.js";
 import { z } from "zod";
+import { ThreadsAPI } from "../../../api/thread.js";''
+import { APIChannel } from "discord-api-types/v10";
+
+
+const RESTInstance = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+const threadsAPI = new ThreadsAPI(RESTInstance);
+const cargoId = "1310664105610444820";
 
 
 
@@ -65,6 +72,71 @@ new Responder({
     type: ResponderType.ModalComponent,
     cache: "cached",
     async run(interaction: ModalSubmitInteraction<"cached">) {
+
+        try {
+            await interaction.deferReply({ ephemeral: true });
+
+            const nameClientInput = interaction.fields.getTextInputValue("nameClient");
+            const portUrgencyInput = interaction.fields.getTextInputValue("portUrgency");
+            const productNameInput = interaction.fields.getTextInputValue("productName");
+            const quantityInput = interaction.fields.getTextInputValue("quantity");
+            const observationsInput = interaction.fields.getTextInputValue("observations");
+
+            const threndTitle = `Solicitar novos produtos - ${interaction.user.globalName}`;
+            const channelThrend = '1296073765859360832';
+
+            const thread = await RESTInstance.post(Routes.threads(channelThrend), {
+                body: {
+                    name: threndTitle,
+                    type: 11,
+                },
+            }) as APIChannel;
+
+            const threadId = thread.id;
+
+            await threadsAPI.addMember(threadId, interaction.user.id);
+
+            const threadChannel = await interaction.client.channels.fetch(threadId) as TextChannel;
+            if (threadChannel) {
+                await threadChannel.send(
+                    `<@&${cargoId}> \nUma nova Solicitação para novos produtos foi feita por **${interaction.user.globalName}**.
+                     \n**Produto:** ${productNameInput}.
+                     \n**Cliente:** ${nameClientInput}
+                     \n**Porte e Urgência:** ${portUrgencyInput}
+                     \n**Quantidade e Local de entrega:** ${quantityInput}
+                     \n**Observação:** ${observationsInput}`
+                );
+            }
+
+
+            const channel = await interaction.client.channels.fetch("1298349477429645352") as TextChannel;
+            if (channel) {
+                await channel.send(
+                    `**Thread criada:** ${threndTitle}\n **Produto:** ${productNameInput}\n **Data de Solicitação:** ${new Date()}`
+                );
+            }
+            await interaction.editReply({
+                content: "Sua solicitação foi registrada com sucesso e a thread foi criada.",
+            })           
+
+        } catch (error) {
+            console.error("Erro ao processar a seleção ou criar thread:", error);
+            if (interaction.replied || interaction.deferred) {
+
+                await interaction.editReply({
+                    content: "Ocorreu um erro ao processar sua solicitação.",
+                });
+            } else {
+
+                await interaction.reply({
+                    content: "Ocorreu um erro ao processar sua solicitação.",
+                    ephemeral: true,
+                });
+            }
+        }
+
+
+
         const nameClientInput = interaction.fields.getTextInputValue("nameClient");
         const portUrgencyInput = interaction.fields.getTextInputValue("portUrgency");
         const productNameInput = interaction.fields.getTextInputValue("productName");
